0c663d0ab257cd32f8264d575ddabb753681059d
aakash <coolakss@yahoo.com>
2021-04-07 23:03:11 -0400

    First program in C++ and class intro

---
+++ Week1/cpp-intro.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Cube.h"
+
+int main() { 
+    int i = 4;
+    i = i+2;
+
+    char x = 'a';
+
+    std::cout << "char is: " << x << " and number is i: " << i << std::endl;
+
+    return 0;
+}
